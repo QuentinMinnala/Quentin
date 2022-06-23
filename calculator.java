da95fcb6b50f44a36b09b4c4650fbce9815030f0
QuentinMinnala <108052777+QuentinMinnala@users.noreply.github.com>
2022-06-23 12:42:43 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,22 @@
+package week1.day1;
+public class calculator
+{
+
+	public int addTwonumbers (int a, int b)
+{
+		return a+b;
+}
+	private Float subTwonumbers (float  f,float g)
+{
+		return f-g;
+}
+	public double mulTwonumbers (double x, double y, double z)
+{
+		return x*y*z;
+}
+	public int divTwonumbers (int a, int b)
+{
+return a/b;
+}
+}
+
