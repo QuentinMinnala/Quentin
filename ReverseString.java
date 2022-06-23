8239190d7a1e1159730de51f507839bff1863ed2
QuentinMinnala <108052777+QuentinMinnala@users.noreply.github.com>
2022-06-23 12:50:31 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class ReverseString {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = "feeling good";
+		char[] arr=test.toCharArray();
+		for(int i=arr.length-1; i>=0;i--)
+		{
+		System.out.println(arr[i]);
+ 
+	}
+	}
+}
